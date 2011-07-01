471fc7ef09e634fd855e2f18f505b5596fbf533d
Crayon2000 <crayon1@rocketmail.com>
2011-07-01 14:22:13 +0000

    *Branch 3D_test* It's working again :)

--- examples/3D_obj/source/main.c
+++ examples/3D_obj/source/main.c
@@ -46,22 +46,22 @@ int main() {
     while(1) {
         GRRLIB_Camera3dSettings(0.0f,0.0f,camZ, 0,1,0, 0,0,0);
 
-        GRRLIB_3dMode(0.1, 1000, 45, 1, model->numtexcoords, model->numnormals);
+        GRRLIB_3dMode(0.1, 1000, 45, model->numtexcoords, model->numnormals);
 
-        if(model->numtexcoords)
+        if(model->numtexcoords) {
             GRRLIB_SetTexture(tex_obj, 0);
+        }
 
         GRRLIB_ObjectView(0, 0, 0, modelRotX, modelRotY, modelRotZ, 1, 1, 1);
 
-        GRRLIB_InitLight(GX_LIGHT0, (guVector){-6, 0, zlight}, 0xFFFFFFFF);
-        GRRLIB_InitLight(GX_LIGHT1, (guVector){ 6, 0, zlight}, 0xFFFFFFFF);
-        GRRLIB_InitLight(GX_LIGHT2, (guVector){ 0,-6, zlight}, 0xFFFFFFFF);
-
-        GRRLIB_LightSwitch(GX_LIGHT0|GX_LIGHT1|GX_LIGHT2, RGBA(Amb,Amb,Amb,0xFF), 0xFFFFFFFF, 0);
+        GRRLIB_SetLightAmbient(RGBA(Amb,Amb,Amb,0xFF));
+        GRRLIB_SetLightDiff(0, (guVector){-6, 0, zlight}, 20.0f, 1.0f, 0xFFFFFFFF);
+        GRRLIB_SetLightDiff(1, (guVector){ 6, 0, zlight}, 20.0f, 1.0f, 0xFFFFFFFF);
+        GRRLIB_SetLightDiff(2, (guVector){ 0,-6, zlight}, 20.0f, 1.0f, 0xFFFFFFFF);
 
         Draw3dObj(model);
 
-        GRRLIB_LightOff();
+        GRRLIB_SetLightOff();
         GRRLIB_2dMode();
         GRRLIB_Printf(rmode->fbWidth/2.0 - (tex_font->tilew*strlen(strCtl1))/2.0, 20,
             tex_font, 0xFFFFFF33, 1, strCtl1);
